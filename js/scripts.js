46b9eeb186a783b902ba53684a37ef83a699afa7
Stephanie Cheng <zombiconvalley@gmail.com>
2020-04-05 22:11:35 -0400

    Large Update

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+  //hamburger menu toggle
+  $('.nav-toggle').click(function() {
+    $('.main-nav').toggleClass('is-open');
+    $('.hamburger').toggleClass('is-open');
+  })
+})
